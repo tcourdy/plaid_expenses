71acdf7ed362b4f3ac7e1728743056f0b8330855
tcourdy <tarik.courdy@gmail.com>
2018-12-29 09:54:26 -0700

    Added helper function

--- notifier.py
+++ notifier.py
@@ -1,9 +1,9 @@
 import plaid
 import json
 import os
+import calendar
 from datetime import datetime, timedelta
 from twilio.rest import Client
-
 
 EXPENSE_FILE_DIR = os.path.dirname(os.path.realpath(__file__));
 
@@ -39,22 +39,31 @@ def get_transactions():
   start_date = end_date - timedelta(days=1)
   response = client.Transactions.get(ACCESS_TOKEN,
                                      start_date=start_date.strftime("%Y-%m-%d"),
-                                     end_date=end_date.strftime("%Y-%m-%d"))
+                                     end_date=end_date.strftime("%Y-%m-%d"),
+                                     count=500)
 
   transactions = response['transactions']
 
-  for t in transactions:
-    category = t['category'][-1]
-    if category in expense_dict:
-      expense_dict[category] = expense_dict[category] + t['amount'];
-    else:
-      expense_dict[category] = t['amount'];
+  expense_dict = parseTransactions(expense_dict, transactions);
 
   with open(expense_file_path, "w+") as fp:
     json.dump(expense_dict, fp, indent=2);
 
   sendText(expense_dict);
 
+
+def parseTransactions(expense_dict, transactions):
+  grand_total = 0
+  for t in transactions:
+    grand_total += t['amount']
+    category = t['category'][-1]
+    if category in expense_dict:
+      expense_dict[category] = expense_dict[category] + t['amount'];
+    else:
+      expense_dict[category] = t['amount'];
+
+  expense_dict["Grand Total"] = grand_total
+  return expense_dict
 
 def sendText(data):
   twilio_client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN);
@@ -63,6 +72,22 @@ def sendText(data):
     from_=TWILIO_PHONE_NUMBER,
     to=MY_PHONE_NUMBER
   );
+
+
+def getMonthlyTotals():
+  current_date = datetime.now();
+  days_in_month = calendar.monthrange(current_date.year, current_date.month); #returns a tuple of (weekday number of first day of month, num_days_in_month)
+  start_date = datetime(current_date.year, current_date.month, 1);
+  end_date = datetime(current_date.year, current_date.month, days_in_month[1])
+  response = client.Transactions.get(ACCESS_TOKEN,
+                                     start_date=start_date.strftime("%Y-%m-%d"),
+                                     end_date=end_date.strftime("%Y-%m-%d"),
+                                     count=500)
+
+  transactions = response['transactions']
+  expense_dict = parseTransactions({}, transactions)
+  print("Transactions for %s -  %s" %(start_date.strftime("%Y-%m-%d"), end_date.strftime("%Y-%m-%d")))
+  print(json.dumps(expense_dict, indent=2))
 
 
 #not used currently
